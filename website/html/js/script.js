40ad6ac9fa2f8ab059356401e3411f84b737c7b4
Jordan Fable <jordan.fable711@gmail.com>
2024-07-17 23:32:01 -0400

    created a navbar for the menu page
    went through so much trouble and effort making an arrow system to go through the options and ended up scraping all of it because making a scroll bar was more intuitive, convienent, and easy. I still think that was the correct choice but it is still frustrating.

--- website/html/js/script.js
+++ website/html/js/script.js
@@ -1,24 +1,61 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//   // Initialize the carousel
+//   $('.customCarousel').carousel({
+//     interval: 4000
+//   });
+
+//   // Handle the carousel thumbnails click
+//   $('[id^=carousel-selector]').click(function() {
+//     var id_selector = $(this).attr('id');
+//     var id = id_selector.substr(id_selector.length - 1);
+//     id = parseInt(id);
+//     $('#myCarousel').carousel(id);
+//     $('[id^=carousel-selector-]').removeClass('selected');
+//     $(this).addClass('selected');
+//   });
+
+//   // When the carousel slides, auto update
+//   $('#myCarousel').on('slid.bs.carousel', function() {
+//     var id = $('.carousel-item.active').data('slide-number');
+//     id = parseInt(id);
+//     $('[id^=carousel-selector-]').removeClass('selected');
+//     $('#carousel-selector-' + id).addClass('selected');
+//   });
+// });
+document.addEventListener('DOMContentLoaded', function() {
   // Initialize the carousel
-  $('.customCarousel').carousel({
-    interval: 4000
+  var carouselElements = document.querySelectorAll('.customCarousel');
+  carouselElements.forEach(function(carouselElement) {
+    new bootstrap.Carousel(carouselElement, {
+      interval: 4000
+    });
   });
 
   // Handle the carousel thumbnails click
-  $('[id^=carousel-selector]').click(function() {
-    var id_selector = $(this).attr('id');
-    var id = id_selector.substr(id_selector.length - 1);
-    id = parseInt(id);
-    $('#myCarousel').carousel(id);
-    $('[id^=carousel-selector-]').removeClass('selected');
-    $(this).addClass('selected');
+  var thumbnailSelectors = document.querySelectorAll('[id^=carousel-selector]');
+  thumbnailSelectors.forEach(function(selector) {
+    selector.addEventListener('click', function() {
+      var id_selector = this.getAttribute('id');
+      var id = id_selector.substr(id_selector.length - 1);
+      id = parseInt(id);
+      var carousel = bootstrap.Carousel.getInstance(document.getElementById('myCarousel'));
+      carousel.to(id);
+      thumbnailSelectors.forEach(function(sel) {
+        sel.classList.remove('selected');
+      });
+      this.classList.add('selected');
+    });
   });
 
   // When the carousel slides, auto update
-  $('#myCarousel').on('slid.bs.carousel', function() {
-    var id = $('.carousel-item.active').data('slide-number');
+  var myCarousel = document.getElementById('myCarousel');
+  myCarousel.addEventListener('slid.bs.carousel', function() {
+    var activeItem = myCarousel.querySelector('.carousel-item.active');
+    var id = activeItem.getAttribute('data-slide-number');
     id = parseInt(id);
-    $('[id^=carousel-selector-]').removeClass('selected');
-    $('#carousel-selector-' + id).addClass('selected');
+    thumbnailSelectors.forEach(function(selector) {
+      selector.classList.remove('selected');
+    });
+    document.getElementById('carousel-selector-' + id).classList.add('selected');
   });
 });
