1d57aa730e55c553417042481415b42b1b20c528
Jordan Fable <jordan.fable711@gmail.com>
2024-08-01 12:19:15 -0400

    made prototye js code for updating the section in menu page
    need to clean up this code a lot.

--- website/html/js/script.js
+++ website/html/js/script.js
@@ -24,16 +24,18 @@
 // });
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+
+  switch (window.location.pathname) {
+    
 ///////////////////////////////////////////
 //                 Home JS               //
 ///////////////////////////////////////////
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Initialize the carousel
-
-  switch (window.location.pathname) {
     case '/index.html':
-      var carouselElements = document.querySelectorAll('.customCarousel');
+    // Initialize the carousel
+    var carouselElements = document.querySelectorAll('.customCarousel');
       carouselElements.forEach(function (carouselElement) {
         new bootstrap.Carousel(carouselElement, {
           interval: 4000
@@ -69,6 +71,10 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById('carousel-selector-' + id).classList.add('selected');
     });
     break;
+///////////////////////////////////////////
+//                 Menu JS               //
+///////////////////////////////////////////
+
     case "/Menu.html":
       const navbarSelector = document.querySelectorAll('li.item a');
 navbarSelector.forEach(function (selector) {
@@ -79,8 +85,67 @@ navbarSelector.forEach(function (selector) {
   this.classList.add('selected');
   });
 });
+
+//every time a section is passed, the navbar highlights the corresponding section
+//if the class "menu-item-group" is passed, remove the "selected" class from all other "li.item a" elements
+//and add the selected class the corresponding section
+
+//So the only thing that remains to do is to check if scrollY > (offsetHeight + offsetTop).
+//If this is true, you passed the element with the scroll.
+//but for us our sections don't have absolute heights since they change and are unique, so
+//we should be subtracting the offsetTop of the first section to the offsetTop of the second section
+//OR we can use the only the offsetHeight of the second section and ignore the first section
+//essentially always looking at the start of the next section instead of the section we are currently on.
+window.addEventListener("scroll", function() {
+  var screenHeight = window.scrollY - document.querySelector("header").scrollHeight;
+  const popularItemsSecHeight = document.getElementById("popular-items").scrollHeight;
+  const sweetsSecHeight = document.getElementById("sweets").scrollHeight + popularItemsSecHeight;
+  const savoriesSecHeight = document.getElementById("savories").scrollHeight + sweetsSecHeight;
+  const cookiesSecHeight = document.getElementById("cookies").scrollHeight + savoriesSecHeight;
+  const strudelsSecHeight = document.getElementById("strudels").scrollHeight + cookiesSecHeight;
+  const specialtiesSecHeight = document.getElementById("specialties").scrollHeight + strudelsSecHeight;
+  if (screenHeight > strudelsSecHeight) {
+      
+      navbarSelector.forEach(function (sel) {
+        sel.classList.remove('selected');
+      });
+      document.querySelector('#nav-specialties a').classList.add('selected');
+  } else if (screenHeight > cookiesSecHeight) {
+      
+    navbarSelector.forEach(function (sel) {
+      sel.classList.remove('selected');
+    });
+    document.querySelector('#nav-strudels a').classList.add('selected');
+ } else if (screenHeight > savoriesSecHeight) {
+      
+  navbarSelector.forEach(function (sel) {
+    sel.classList.remove('selected');
+  });
+  document.querySelector('#nav-cookies a').classList.add('selected');
+} else if (screenHeight > sweetsSecHeight) {
+      
+  navbarSelector.forEach(function (sel) {
+    sel.classList.remove('selected');
+  });
+  document.querySelector('#nav-savories a').classList.add('selected');
+ } else if (screenHeight > popularItemsSecHeight) {
+      
+  navbarSelector.forEach(function (sel) {
+    sel.classList.remove('selected');
+  });
+  document.querySelector('#nav-sweets a').classList.add('selected');
+} else {
+
+  navbarSelector.forEach(function (sel) {
+    sel.classList.remove('selected');
+  });
+  document.querySelector('#nav-popular-items a').classList.add('selected');
+ }
+});
     break;
-    
+    case "/About.html":
+      break;
+
     default:
       break;
     
@@ -88,9 +153,6 @@ navbarSelector.forEach(function (selector) {
   }
  
 
-///////////////////////////////////////////
-//                 Menu JS               //
-///////////////////////////////////////////
 
 
 });
