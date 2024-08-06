18dfb26047e911ff4dd179540a407d3c2c95040f
Jordan Fable <jordan.fable711@gmail.com>
2024-08-06 12:52:32 -0400

    worked on small criticisms
    need to figure out why about page nav doesn't underline

--- website/html/js/script.js
+++ website/html/js/script.js
@@ -26,22 +26,31 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  
+
+  if (window.location.hash) {
+    // Remove the fragment identifier
+    history.replaceState(null, null, ' ');
+  }
+  const navItems = document.querySelectorAll('.nav-item a');
+  navItems.forEach(function (sel) {
+    sel.classList.remove('nav-selected');
+  })
 
   switch (window.location.pathname) {
-    
-///////////////////////////////////////////
-//                 Home JS               //
-///////////////////////////////////////////
+
+    ///////////////////////////////////////////
+    //                 Home JS               //
+    ///////////////////////////////////////////
     case '/index.html':
-    // Initialize the carousel
-    var carouselElements = document.querySelectorAll('.customCarousel');
+      document.querySelector('#nav-home a').classList.add('nav-selected');
+      // Initialize the carousel
+      var carouselElements = document.querySelectorAll('.customCarousel');
       carouselElements.forEach(function (carouselElement) {
         new bootstrap.Carousel(carouselElement, {
           interval: 4000
         });
       });
-    
+
       // Handle the carousel thumbnails click
       var thumbnailSelectors = document.querySelectorAll('[id^=carousel-selector]');
       thumbnailSelectors.forEach(function (selector) {
@@ -57,128 +66,75 @@ document.addEventListener('DOMContentLoaded', function () {
           this.classList.add('selected');
         });
       });
-    
-    
-    // When the carousel slides, auto update
-    var myCarousel = document.getElementById('myCarousel');
-    myCarousel.addEventListener('slid.bs.carousel', function () {
-      var activeItem = myCarousel.querySelector('.carousel-item.active');
-      var id = activeItem.getAttribute('data-slide-number');
-      id = parseInt(id);
-      thumbnailSelectors.forEach(function (selector) {
-        selector.classList.remove('selected');
+
+
+      // When the carousel slides, auto update
+      var myCarousel = document.getElementById('myCarousel');
+      myCarousel.addEventListener('slid.bs.carousel', function () {
+        var activeItem = myCarousel.querySelector('.carousel-item.active');
+        var id = activeItem.getAttribute('data-slide-number');
+        id = parseInt(id);
+        thumbnailSelectors.forEach(function (selector) {
+          selector.classList.remove('selected');
+        });
+        document.getElementById('carousel-selector-' + id).classList.add('selected');
       });
-      document.getElementById('carousel-selector-' + id).classList.add('selected');
-    });
-    break;
-///////////////////////////////////////////
-//                 Menu JS               //
-///////////////////////////////////////////
+      break;
+    ///////////////////////////////////////////
+    //                 Menu JS               //
+    ///////////////////////////////////////////
 
     case "/Menu.html":
+      document.querySelector('#nav-menu a').classList.add('nav-selected');
+    //if a navbar button is clicked, highlight it
       const navbarSelector = document.querySelectorAll('li.item a');
-navbarSelector.forEach(function (selector) {
-  selector.addEventListener('click', function () {
-  navbarSelector.forEach(function (sel) {
-    sel.classList.remove('selected');
-  });
-  this.classList.add('selected');
-  });
-});
+      // navbarSelector.forEach(function (selector) {
+      //   selector.addEventListener('click', function () {
+      //     navbarSelector.forEach(function (sel) {
+      //       sel.classList.remove('selected');
+      //     });
+      //     this.classList.add('selected');
+      //   });
+      // });
 
+      //if the user scrolls, highlight the corresponding navbar button
+      let observer = new IntersectionObserver((entries, observer) => {
+        let topMostEntry = null;
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
 
-//every time a section is passed, the navbar highlights the corresponding section
-//if the class "menu-item-group" is passed, remove the "selected" class from all other "li.item a" elements
-//and add the selected class the corresponding section
-
-let observer = new IntersectionObserver((entries, observer) => {
-  let topMostEntry = null;
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // Get the target element that is currently in view
-      
-      if (!topMostEntry || entry.boundingClientRect.top < topMostEntry.boundingClientRect.top) {
-        topMostEntry = entry;
-      }
-    }
-    if (topMostEntry) {
-      let targetElement = topMostEntry.target;
-      navbarSelector.forEach(function (sel) {
-               sel.classList.remove('selected');
-            });
-      document.querySelector("#nav-" + targetElement.id + " a").classList.add('selected');
+            if (!topMostEntry || entry.boundingClientRect.top < topMostEntry.boundingClientRect.top) {
+              topMostEntry = entry;
+            }
           }
-  });
-}, { threshold: 0.22 });
-//So the only thing that remains to do is to check if scrollY > (offsetHeight + offsetTop).
-//If this is true, you passed the element with the scroll.
-//but for us our sections don't have absolute heights since they change and are unique, so
-//we should be subtracting the offsetTop of the first section to the offsetTop of the second section
-//OR we can use the only the offsetHeight of the second section and ignore the first section
-//essentially always looking at the start of the next section instead of the section we are currently on.
-window.addEventListener("scroll", function() {
-  // var screenHeight = window.scrollY - document.querySelector("header").scrollHeight;
-  // const popularItemsSecHeight = document.getElementById("popular-items").scrollHeight;
-  // const sweetsSecHeight = document.getElementById("sweets").scrollHeight + popularItemsSecHeight;
-  // const savoriesSecHeight = document.getElementById("savories").scrollHeight + sweetsSecHeight;
-  // const cookiesSecHeight = document.getElementById("cookies").scrollHeight + savoriesSecHeight;
-  // const strudelsSecHeight = document.getElementById("strudels").scrollHeight + cookiesSecHeight;
-  // const specialtiesSecHeight = document.getElementById("specialties").scrollHeight + strudelsSecHeight;
-//   if (screenHeight > strudelsSecHeight) {
-      
-//       navbarSelector.forEach(function (sel) {
-//         sel.classList.remove('selected');
-//       });
-//       document.querySelector('#nav-specialties a').classList.add('selected');
-//   } else if (screenHeight > cookiesSecHeight) {
-      
-//     navbarSelector.forEach(function (sel) {
-//       sel.classList.remove('selected');
-//     });
-//     document.querySelector('#nav-strudels a').classList.add('selected');
-//  } else if (screenHeight > savoriesSecHeight) {
-      
-//   navbarSelector.forEach(function (sel) {
-//     sel.classList.remove('selected');
-//   });
-//   document.querySelector('#nav-cookies a').classList.add('selected');
-// } else if (screenHeight > sweetsSecHeight) {
-      
-//   navbarSelector.forEach(function (sel) {
-//     sel.classList.remove('selected');
-//   });
-//   document.querySelector('#nav-savories a').classList.add('selected');
-//  } else if (screenHeight > popularItemsSecHeight) {
-      
-//   navbarSelector.forEach(function (sel) {
-//     sel.classList.remove('selected');
-//   });
-//   document.querySelector('#nav-sweets a').classList.add('selected');
-// } else {
+          if (topMostEntry) {
+            let targetElement = topMostEntry.target;
+            navbarSelector.forEach(function (sel) {
+              sel.classList.remove('selected');
+            });
+            document.querySelector("#nav-" + targetElement.id + " a").classList.add('selected');
+          }
+        });
+      }, { threshold: 0.23 }); //test for best value
 
-//   navbarSelector.forEach(function (sel) {
-//     sel.classList.remove('selected');
-//   });
-//   document.querySelector('#nav-popular-items a').classList.add('selected');
-//  }
+      window.addEventListener("scroll", function () {
 
-let targets = document.querySelectorAll('section');
-
-targets.forEach(target => {
-  observer.observe(target);
-});
-
-});
-    break;
+        let targets = document.querySelectorAll('section');
+        targets.forEach(target => {
+          observer.observe(target);
+        });
+      });
+      break;
     case "/About.html":
+      document.querySelector('#nav-about a').classList.add('nav-selected');
       break;
 
     default:
       break;
-    
-  
+
+
   }
- 
+
 
 
 
