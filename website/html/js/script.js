ee36d26e8b26218fee2ebf73c02bfeab2c0a8644
Jordan Fable <jordan.fable711@gmail.com>
2024-08-07 01:42:43 -0400

    I FIXED IT
    i hated this

--- website/html/js/script.js
+++ website/html/js/script.js
@@ -23,71 +23,74 @@
 //   });
 // });
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener("DOMContentLoaded", function () {
   if (window.location.hash) {
-    // Remove the fragment identifier
-    history.replaceState(null, null, ' ');
+    // removes the hash from the URL
+    history.replaceState(null, null, " ");
   }
-  const navItems = document.querySelectorAll('.nav-item a');
+
+  //removes the underline from all nav items
+  const navItems = document.querySelectorAll(".nav-item a");
   navItems.forEach(function (sel) {
-    sel.classList.remove('nav-selected');
-  })
+    sel.classList.remove("nav-selected");
+  });
 
   switch (window.location.pathname) {
-
     ///////////////////////////////////////////
     //                 Home JS               //
     ///////////////////////////////////////////
-    case '/index.html':
-      document.querySelector('#nav-home a').classList.add('nav-selected');
+    case "/index.html":
+      document.querySelector("#nav-home a").classList.add("nav-selected");
       // Initialize the carousel
-      var carouselElements = document.querySelectorAll('.customCarousel');
+      var carouselElements = document.querySelectorAll(".customCarousel");
       carouselElements.forEach(function (carouselElement) {
         new bootstrap.Carousel(carouselElement, {
-          interval: 4000
+          interval: 4000,
         });
       });
 
       // Handle the carousel thumbnails click
-      var thumbnailSelectors = document.querySelectorAll('[id^=carousel-selector]');
+      var thumbnailSelectors = document.querySelectorAll(
+        "[id^=carousel-selector]"
+      );
       thumbnailSelectors.forEach(function (selector) {
-        selector.addEventListener('click', function () {
-          var id_selector = this.getAttribute('id');
+        selector.addEventListener("click", function () {
+          var id_selector = this.getAttribute("id");
           var id = id_selector.substr(id_selector.length - 1);
           id = parseInt(id);
-          var carousel = bootstrap.Carousel.getInstance(document.getElementById('myCarousel'));
+          var carousel = bootstrap.Carousel.getInstance(
+            document.getElementById("myCarousel")
+          );
           carousel.to(id);
           thumbnailSelectors.forEach(function (sel) {
-            sel.classList.remove('selected');
+            sel.classList.remove("selected");
           });
-          this.classList.add('selected');
+          this.classList.add("selected");
         });
       });
-
 
       // When the carousel slides, auto update
-      var myCarousel = document.getElementById('myCarousel');
-      myCarousel.addEventListener('slid.bs.carousel', function () {
-        var activeItem = myCarousel.querySelector('.carousel-item.active');
-        var id = activeItem.getAttribute('data-slide-number');
+      var myCarousel = document.getElementById("myCarousel");
+      myCarousel.addEventListener("slid.bs.carousel", function () {
+        var activeItem = myCarousel.querySelector(".carousel-item.active");
+        var id = activeItem.getAttribute("data-slide-number");
         id = parseInt(id);
         thumbnailSelectors.forEach(function (selector) {
-          selector.classList.remove('selected');
+          selector.classList.remove("selected");
         });
-        document.getElementById('carousel-selector-' + id).classList.add('selected');
+        document
+          .getElementById("carousel-selector-" + id)
+          .classList.add("selected");
       });
+
       break;
     ///////////////////////////////////////////
     //                 Menu JS               //
     ///////////////////////////////////////////
-
     case "/Menu.html":
-      document.querySelector('#nav-menu a').classList.add('nav-selected');
-    //if a navbar button is clicked, highlight it
-      const navbarSelector = document.querySelectorAll('li.item a');
+      document.querySelector("#nav-menu a").classList.add("nav-selected");
+      //if a navbar button is clicked, highlight it
+      const navbarSelector = document.querySelectorAll("li.item a");
       // navbarSelector.forEach(function (selector) {
       //   selector.addEventListener('click', function () {
       //     navbarSelector.forEach(function (sel) {
@@ -98,44 +101,47 @@ document.addEventListener('DOMContentLoaded', function () {
       // });
 
       //if the user scrolls, highlight the corresponding navbar button
-      let observer = new IntersectionObserver((entries, observer) => {
-        let topMostEntry = null;
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-
-            if (!topMostEntry || entry.boundingClientRect.top < topMostEntry.boundingClientRect.top) {
-              topMostEntry = entry;
+      let observer = new IntersectionObserver(
+        (entries, observer) => {
+          let topMostEntry = null;
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              if (
+                !topMostEntry ||
+                entry.boundingClientRect.top <
+                  topMostEntry.boundingClientRect.top
+              ) {
+                topMostEntry = entry;
+              }
             }
-          }
-          if (topMostEntry) {
-            let targetElement = topMostEntry.target;
-            navbarSelector.forEach(function (sel) {
-              sel.classList.remove('selected');
-            });
-            document.querySelector("#nav-" + targetElement.id + " a").classList.add('selected');
-          }
-        });
-      }, { threshold: 0.23 }); //test for best value
+            if (topMostEntry) {
+              let targetElement = topMostEntry.target;
+              navbarSelector.forEach(function (sel) {
+                sel.classList.remove("selected");
+              });
+              document
+                .querySelector("#nav-" + targetElement.id + " a")
+                .classList.add("selected");
+            }
+          });
+        },
+        { threshold: 0.23 }
+      ); //test for best value
 
       window.addEventListener("scroll", function () {
-
-        let targets = document.querySelectorAll('section');
-        targets.forEach(target => {
+        let targets = document.querySelectorAll("section");
+        targets.forEach((target) => {
           observer.observe(target);
         });
       });
+
       break;
-    case "/About.html":
-      document.querySelector('#nav-about a').classList.add('nav-selected');
+
+    case "/about.html":
+      document.querySelector("#nav-about a").classList.add("nav-selected");
       break;
 
     default:
       break;
-
-
   }
-
-
-
-
 });
